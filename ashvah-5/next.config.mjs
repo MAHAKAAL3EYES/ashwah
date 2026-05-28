e0b58580b7b480552ab60cd9ea2bb0db3384f587
MAHAKAAL3EYES <82955325+MAHAKAAL3EYES@users.noreply.github.com>
2026-05-28 23:02:23 +0530

    Update next.config.mjs

--- ashvah-5/next.config.mjs
+++ ashvah-5/next.config.mjs
@@ -1,20 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.supabase.co",
+        hostname: "www.ashvah.in",
+        pathname: "/assets/uploads/**",
       },
       {
         protocol: "https",
-        hostname: "www.ashvah.in",
+        hostname: "ashvah.in",
+        pathname: "/assets/uploads/**",
       },
     ],
-  },
-  experimental: {
-    optimizePackageImports: ["lucide-react", "framer-motion"],
   },
 };
 
